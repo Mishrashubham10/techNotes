194a399223f92fd2a35c599e3ae496253a171f2a
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-11-27 20:51:00 +0530

    Create apiSlices for notes and users

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Public from './components/Public'
-import Login from './features/auth/Login'
-import DashLayout from './components/DashLayout'
-import Welcome from './features/auth/Welcome'
-import NotesList from './features/notes/NotesList'
-import UsersList from './features/users/UsersList'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Public from './components/Public';
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout';
+import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
 
 const App = () => {
   return (
@@ -17,20 +17,20 @@ const App = () => {
         <Route path="/dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
           {/* Note route start */}
-          <Route path='/notes' element={<NotesList />}>
+          <Route path="notes" element={<NotesList />}>
             <Route index element={<NotesList />} />
           </Route>
           {/* Note route end */}
           {/* User route start */}
-          <Route path='/users' element={<UsersList />}>
-          <Route index element={<UsersList />} />
+          <Route path="users" element={<UsersList />}>
+            <Route index element={<UsersList />} />
           </Route>
           {/* User route end */}
         </Route>
         {/* Dash end */}
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
