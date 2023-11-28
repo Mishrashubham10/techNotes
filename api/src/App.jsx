4f921f985afdd05446a227b7b9556c7bccb67756
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-11-28 10:22:30 +0530

    Create edit, update and delete user form

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -6,6 +6,10 @@ import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
 import NotesList from './features/notes/NotesList';
 import UsersList from './features/users/UsersList';
+import EditUser from './features/users/EditUser';
+import NewUserForm from './features/users/NewUserForm';
+import NewNote from './features/notes/NewNote';
+import EditNote from './features/notes/EditNote';
 
 const App = () => {
   return (
@@ -16,16 +20,20 @@ const App = () => {
         {/* Dash start */}
         <Route path="/dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
-          {/* Note route start */}
-          <Route path="notes" element={<NotesList />}>
-            <Route index element={<NotesList />} />
-          </Route>
-          {/* Note route end */}
           {/* User route start */}
-          <Route path="users" element={<UsersList />}>
-            <Route index element={<UsersList />} />
-          </Route>
+          <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
           {/* User route end */}
+          {/* Note route start */}
+          <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
+          {/* Note route end */}
         </Route>
         {/* Dash end */}
       </Route>
